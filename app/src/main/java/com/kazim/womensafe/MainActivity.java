69a1d10d7f66046377def75d0ea9477faa85a1bc
abhishekgupta368 <abhigupta9087@gmail.com>
2020-07-01 00:16:56 +0530

    Splash Screen

--- app/src/main/java/com/kazim/womensafe/MainActivity.java
+++ app/src/main/java/com/kazim/womensafe/MainActivity.java
@@ -2,13 +2,33 @@ package com.kazim.womensafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView logo;
+    private static int SPLASH_SCREEN_TIME_OUT=2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = (ImageView)findViewById(R.id.logo);
+        Animation aniFade = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        logo.startAnimation(aniFade);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent UserLogin=new Intent(MainActivity.this,UserLogin.class);
+                startActivity(UserLogin);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+
     }
 }
